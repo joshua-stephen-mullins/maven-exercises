7a9accc4e6baa7c030b950daa81e66fe3499f575
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2023-01-12 15:07:37 -0600

    finished dependencies exercise

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,6 +1,21 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 public class NewClass {
 
     public static void main(String[] args) {
-        String josh = "josh";
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("enter a sentence: ");
+        String userInput = scanner.nextLine();
+
+        System.out.println(StringUtils.swapCase(userInput));
+        System.out.println(StringUtils.reverse(userInput));
+        System.out.println(StringUtils.isNumeric(userInput));
+
+
     }
 }
